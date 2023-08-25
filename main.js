8470b86d1c6279db79cce297932288cafff6e593
Avnish Bharadva <avnishbharadva@gmail.com>
2023-08-25 19:06:23 +0530

    Basic structure complete

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_amLUC70aocLM8RhqtQS8wx73Te21SN3e2Iu67VjZ"
 
+const btn = document.getElementById("btn")
 const tableBody = document.querySelector('tbody')
 tableBody.innerHTML = `
     <td>hbf</td>
